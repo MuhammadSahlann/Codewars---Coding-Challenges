0a124010a582f1925dfc1c7e5e0c7b2dbed07b4b
Muhammad Sahlan <muhammadsahlann@gmail.com>
2025-07-23 16:03:59 +0700

    update 7-gradingStudentsSolution.js

--- HackerRank challanges/7-gradingStudentsSolution.js
+++ HackerRank challanges/7-gradingStudentsSolution.js
@@ -2,16 +2,31 @@
 
 // https://www.hackerrank.com/challenges/grading/problem
 
+//function gradingStudents(grades) {
+//    for (let i = 0; i < grades.length; i++) {
+//        let grade = Math.ceil(grades[i] / 5) * 5;
+//
+//        if (grade >= 40) {
+//            if (grade - grades[i] < 3) grades[i] += grade - grades[i];
+//        }
+//    }
+//
+//    return grades
+//}
+
 function gradingStudents(grades) {
     for (let i = 0; i < grades.length; i++) {
-        let grade = Math.ceil(grades[i] / 5) * 5;
+        let originalGrade = grades[i];
 
-        if (grade >= 40) {
-            if (grade - grades[i] < 3) grades[i] += grade - grades[i];
+        if (originalGrade >= 38) {
+            let nextMultipleOf5 = Math.ceil(originalGrade / 5) * 5;
+            if (nextMultipleOf5 - originalGrade < 3) {
+                grades[i] = nextMultipleOf5;
+            }
         }
     }
 
-    return grades
+    return grades;
 }
 
 console.log(gradingStudents([ 73, 67, 38, 33 ]));
