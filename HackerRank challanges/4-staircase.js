4e283a428f64a3f25023de99b78cd58dcd62c573
muhammad sahlan <muhammadsahlann@gmail.com>
2025-07-25 06:17:50 +0700

    update

--- HackerRank challanges/4-staircase.js
+++ HackerRank challanges/4-staircase.js
@@ -22,3 +22,35 @@ function staircase(n) {
 }
 
 staircase(6);
+
+
+    // POLA SEGITIGA
+
+    var    n      = 5; // jumlah baris
+    var result = '';
+    
+    // buat baris
+    for (var row = 1; row <= n; row++) {
+    
+      // cetak spasi
+      for (var space = 0; space < n - row; space++) {
+        result += ' ';
+      }
+      
+      // buat pola
+      for (var star = 0; star < row; star++) {
+        
+        // jika nilai var star lebih dari atau sama dengan 1 tambahkan simbol tambah (+) setelahnya 
+        if (star >= 1) {
+            result += '+'
+        }
+        
+        // selain dari itu langsung cetak *
+        result += '*';
+      }
+      
+      // print row
+      result += '\n';
+    }
+    
+    console.log(result);
